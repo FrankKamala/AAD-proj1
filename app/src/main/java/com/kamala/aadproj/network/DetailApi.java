8b2a234b17c808b505e9028abd5562a197a74bad
FrankKamala <frankkamala@gmail.com>
2020-09-03 14:20:55 +0300

    Add Get on Interface

--- app/src/main/java/com/kamala/aadproj/network/DetailApi.java
+++ app/src/main/java/com/kamala/aadproj/network/DetailApi.java
@@ -1,4 +1,17 @@
 package com.kamala.aadproj.network;
 
+import com.kamala.aadproj.models.Hours;
+import com.kamala.aadproj.models.Skill;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface DetailApi {
+    @GET("hours")
+    Call<List<Hours>>getDetailHours();
+    @GET("skilliq")
+    Call<List<Skill>>getDetailSkill();
+
 }
