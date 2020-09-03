e0b44fc965e08367b6d461d692b1210176964930
FrankKamala <frankkamala@gmail.com>
2020-09-03 14:33:05 +0300

    set up builder

--- app/src/main/java/com/kamala/aadproj/constants/Constants.java
+++ app/src/main/java/com/kamala/aadproj/constants/Constants.java
@@ -1,6 +1,6 @@
 package com.kamala.aadproj.constants;
 
 public class Constants {
-    public static final String HOURS_SKILL_URL = "https://gadsapi.herokuapp.com";
+    public static final String HOURS_SKILL_URL = "https://gadsapi.herokuapp.com/api/";
 
 }
--- app/src/main/java/com/kamala/aadproj/network/DetailClient.java
+++ app/src/main/java/com/kamala/aadproj/network/DetailClient.java
@@ -1,4 +1,32 @@
 package com.kamala.aadproj.network;
 
+import com.kamala.aadproj.constants.Constants;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class DetailClient {
+    private static Retrofit retrofit = null;
+    public static DetailApi getClient(){
+        if (retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(chain -> {
+                        Request newRequest  = chain.request().newBuilder()
+                                .build();
+                        return chain.proceed(newRequest);
+                    })
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(Constants.HOURS_SKILL_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit.create(DetailApi.class);
+    }
+
 }
