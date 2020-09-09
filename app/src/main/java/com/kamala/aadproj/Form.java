120e1fbcdd651ba73c9e417f24b404db78ef396e
FrankKamala <frankkamala@gmail.com>
2020-09-09 14:40:15 +0300

    bind views

--- app/src/main/java/com/kamala/aadproj/Form.java
+++ app/src/main/java/com/kamala/aadproj/Form.java
@@ -3,8 +3,22 @@ package com.kamala.aadproj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
 
 public class Form extends AppCompatActivity {
+    @BindView(R.id.secondName)
+    EditText mSName;
+    @BindView(R.id.firstName)
+    EditText mFName;
+    @BindView(R.id.subEmail)
+    EditText mMail;
+    @BindView(R.id.subLink)
+    EditText mLink;
+    @BindView(R.id.subButton)
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
