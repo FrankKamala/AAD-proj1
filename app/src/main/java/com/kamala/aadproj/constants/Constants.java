d13e64efa2c175c4a82283b5d1683612b6984b2b
FrankKamala <frankkamala@gmail.com>
2020-09-09 14:36:07 +0300

    add form Entries

--- app/src/main/java/com/kamala/aadproj/constants/Constants.java
+++ app/src/main/java/com/kamala/aadproj/constants/Constants.java
@@ -2,5 +2,13 @@ package com.kamala.aadproj.constants;
 
 public class Constants {
     public static final String HOURS_SKILL_URL = "https://gadsapi.herokuapp.com/api/";
+    public static final String FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse";
+
+    //form
+    public static final String email = "entry.1824927963";
+    public static final String fName = "entry.1877115667";
+    public static final String lName = "entry.2006916086";
+    public static final String linkPro = "entry.284483984";
+
 
 }
