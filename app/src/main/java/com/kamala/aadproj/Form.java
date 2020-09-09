b09e381b23ba89edef19aa0df1354a97acd21560
FrankKamala <frankkamala@gmail.com>
2020-09-09 14:44:15 +0300

    bind views

--- app/src/main/java/com/kamala/aadproj/Form.java
+++ app/src/main/java/com/kamala/aadproj/Form.java
@@ -1,12 +1,16 @@
 package com.kamala.aadproj;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toolbar;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class Form extends AppCompatActivity {
     @BindView(R.id.secondName)
@@ -20,9 +24,11 @@ public class Form extends AppCompatActivity {
     @BindView(R.id.subButton)
     Button mButton;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+        ButterKnife.bind(this);
     }
 }
