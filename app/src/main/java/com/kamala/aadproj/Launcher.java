3391414dc7f47713ff46014545f75636d942a854
FrankKamala <frankkamala@gmail.com>
2020-09-10 15:37:12 +0300

    set up launcher

--- app/src/main/java/com/kamala/aadproj/Launcher.java
+++ app/src/main/java/com/kamala/aadproj/Launcher.java
@@ -2,7 +2,9 @@ package com.kamala.aadproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Launcher extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Launcher extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+        Handler handler =new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Launcher.this,MainActivity.class)); //starts main Activity
+                finish();
+
+            }
+        },4000);
     }
 }
